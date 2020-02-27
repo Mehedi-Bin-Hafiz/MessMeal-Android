04d0a8ccdf212909716951ff65176941441574bc
mehedibinhafiz <mehedibinhafiz@gmail.com>
2020-02-28 01:08:07 +0600

    Expense added

--- app/src/main/java/com/mehedibinhafiz/messmeal/Expense.java
+++ app/src/main/java/com/mehedibinhafiz/messmeal/Expense.java
@@ -3,12 +3,17 @@ package com.mehedibinhafiz.messmeal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class Expense extends AppCompatActivity {
+    WebView link;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expense);
+        link=findViewById(R.id.webexpense);
+        link.getSettings().setJavaScriptEnabled(true);
+        link.loadUrl("http://quiet-wildwood-33076.herokuapp.com/edash/");
     }
 }
